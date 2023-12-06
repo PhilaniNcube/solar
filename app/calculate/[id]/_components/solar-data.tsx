86723fb6c0417ba5eb765c452ddacababe7b9170
Philani Ncube <ncbphi001@gmail.com>
2023-12-06 13:11:39 +0200

    Refactor solar-data component layout

--- app/calculate/[id]/_components/solar-data.tsx
+++ app/calculate/[id]/_components/solar-data.tsx
@@ -28,68 +28,8 @@ const SolarData = async ({lat, lng}:SolarDataProps) => {
   return (
     <section className="container py-6">
       <div className="grid grid-cols-5 gap-4">
-        <div className="col-span-1">
-          <div>
-            <Card className="p-4 bg-white dark:bg-gray-800 rounded-lg shadow-md space-y-4">
-              <div className="flex items-center space-x-4">
-                <HomeIcon className="h-8 w-8 text-gray-500 dark:text-gray-400" />
-                <div>
-                  <CardTitle className="text-lg font-bold text-gray-900 dark:text-gray-100">
-                    Roof Area
-                  </CardTitle>
-                  <CardDescription className="text-gray-500 dark:text-gray-400">
-                    {solarPotential.maxArrayAreaMeters2.toFixed(2)} m
-                    <sup>2</sup>
-                  </CardDescription>
-                </div>
-              </div>
-            </Card>
-            <Card className="p-4 bg-white dark:bg-gray-800 rounded-lg shadow-md space-y-4">
-              <div className="flex items-center space-x-4">
-                <ShieldQuestion className="h-8 w-8 text-gray-500 dark:text-gray-400" />
-                <div>
-                  <CardTitle className="text-lg font-bold text-gray-900 dark:text-gray-100">
-                    Solar Output
-                  </CardTitle>
-                  <CardDescription className="text-gray-500 dark:text-gray-400">
-                    {(
-                      solarPotential.solarPanelConfigs[0].yearlyEnergyDcKwh *
-                      solarOutputRatio
-                    ).toFixed(2)}{" "}
-                    kWh
-                  </CardDescription>
-                </div>
-              </div>
-            </Card>
-            <Card className="p-4 bg-white dark:bg-gray-800 rounded-lg shadow-md space-y-4">
-              <div className="flex items-center space-x-4">
-                <LockKeyhole className="h-8 w-8 text-gray-500 dark:text-gray-400" />
-                <div>
-                  <CardTitle className="text-lg font-bold text-gray-900 dark:text-gray-100">
-                    Services
-                  </CardTitle>
-                  <CardDescription className="text-gray-500 dark:text-gray-400">
-                    What we offer
-                  </CardDescription>
-                </div>
-              </div>
-            </Card>
-            <Card className="p-4 bg-white dark:bg-gray-800 rounded-lg shadow-md space-y-4">
-              <div className="flex items-center space-x-4">
-                <Link className="h-8 w-8 text-gray-500 dark:text-gray-400" />
-                <div>
-                  <CardTitle className="text-lg font-bold text-gray-900 dark:text-gray-100">
-                    Contact
-                  </CardTitle>
-                  <CardDescription className="text-gray-500 dark:text-gray-400">
-                    Get in touch with us
-                  </CardDescription>
-                </div>
-              </div>
-            </Card>
-          </div>
-        </div>
-        <div className="col-span-4 bg-slate-300 min-h-[600px] rounded-lg p-8 shadow-lg">
+
+        <div className="col-span-5 bg-slate-300 min-h-[600px] rounded-lg p-8 shadow-lg">
           {/*  */}
           <p className="font-medium text-slate-800">
             How many solar panels configurations are available at this address?{" "}
