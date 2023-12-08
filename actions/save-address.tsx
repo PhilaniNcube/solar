a449380aa4449eea1cb94b3321b992c7500e260a
Philani Ncube <ncbphi001@gmail.com>
2023-12-08 20:57:14 +0200

    Add Google Places Autocomplete to AddressForm component

---
+++ actions/save-address.tsx
@@ -0,0 +1,98 @@
+"use server";
+
+import { GeocodingResponse } from "@/interfaces";
+import { createClient } from "@/utils/supabase/server";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+
+
+export const saveAddress = async (formData: FormData) => {
+
+  const name = (formData.get("name") as string) || "";
+  const phone = (formData.get("phone") as string) || "";
+  const address = formData.get("address") as string;
+  const email = (formData.get("email") as string) || "";
+  const installation = (formData.get("installation") as string) || "";
+  const electricity_bill = (formData.get("electricity-bill") as string) || "";
+
+  const cookieStore = cookies();
+  const supabase = createClient(cookieStore);
+
+  if (
+    typeof electricity_bill !== "string" ||
+    typeof name !== "string" ||
+    typeof phone !== "string" ||
+    typeof address !== "string" ||
+    typeof email !== "string" ||
+    typeof installation !== "string"
+  ) {
+    throw new Error("Invalid data");
+  }
+
+  const { data: lead, error } = await supabase
+    .from("leads")
+    .insert([
+      {
+        name : "",
+        email : "",
+        phone : "",
+        address : "",
+        electricity_bill: 0,
+        installation,
+      },
+    ])
+    .select("*")
+    .single();
+
+  console.log({ lead, error });
+
+  let encodedAddress;
+
+  if (typeof address !== "string") {
+    return;
+  } else {
+    encodedAddress = encodeURIComponent(address);
+  }
+
+  const gecodingUrl = new URL(
+    `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}&key=${process.env.NEXT_PUBLIC_MAPS_API_KEY}`
+  );
+
+  const response = await fetch(gecodingUrl)
+    .then((res) => res.json())
+    .then((data) => data)
+    .catch((err) => console.error(err));
+
+  if (!response || response.status === "ZERO_RESULTS") {
+    redirect(`/error`);
+    // throw new Error("Could not find location for the specified address.");
+  }
+
+  const data: GeocodingResponse = await response;
+
+  const { data: addressResults, error: addressError } = await supabase
+    .from("geocoding")
+    .insert([
+      {
+        results: data.results,
+        status: data.status,
+        lead_id: lead?.id,
+      },
+    ])
+    .select("*")
+    .single();
+
+  if (addressError) {
+    // throw new Error("Could not save address")
+    redirect(`/error`);
+  }
+
+  if (!addressResults) {
+    // throw new Error("Could not save address")
+    redirect(`/error`);
+  }
+
+  redirect(`/calculate/${addressResults.id}`);
+
+  // redirect(`/calculate?lat=${data.results[0].geometry.location.lat}&lng=${data.results[0].geometry.location.lng}&address=${data.results[0].formatted_address}`)
+};
--- components/home-page/address-form.tsx
+++ components/home-page/address-form.tsx
@@ -1,3 +1,6 @@
+"use client"
+
+import GooglePlacesAutocomplete from "react-google-places-autocomplete";
 import { createClient } from "@/utils/supabase/server";
 import { cookies } from "next/headers";
 import { Label } from "@/components/ui/label";
@@ -9,96 +12,41 @@ import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
 import { GeocodingResponse } from "@/interfaces";
 import { Battery, BatteryChargingIcon, Grid3X3, Home, Link2OffIcon, PlusIcon, UtilityPole } from "lucide-react";
 import SubmitButton from "../submit-button";
-import  Autocomplete  from "./autocomplete";
+import { saveAddress } from "@/actions/save-address";
+import { useState } from "react";
 
-const AddressForm = async () => {
-
-  const saveAddress = async (formData:FormData) => {
-    "use server"
-
-     const name = formData.get("name") as string || "";
-     const phone = formData.get("phone") as string || "";
-     const address = formData.get("address") as string;
-     const email = formData.get("email") as string || "";
-     const installation = formData.get("installation") as string || "";
-     const electricity_bill = formData.get("electricity-bill") as string  || "";
-
-    const cookieStore = cookies()
-    const supabase = createClient(cookieStore)
-
-
-    if(typeof electricity_bill !== "string" || typeof name !== "string" || typeof phone !== "string" || typeof address !== "string" || typeof email !== "string" || typeof installation !== "string") {
-      throw new Error("Invalid data")
+type AutoCompleteType = {
+  label: string
+  value: {
+    matched_substrings: {
+      length: number
+      offset: number
+    }[]
+    description: string
+    place_id: string
+    reference: string
+    structured_formatting: {
+      main_text: string
+      secondary_text: string
+      main_text_substring: {
+        offset: number
+        length: number
+      }[]
     }
-
-
-
-const { data: lead, error } = await supabase
-  .from("leads")
-  .insert([
-    {
-      name,
-      email,
-      phone,
-      address,
-      electricity_bill: Number(electricity_bill),
-      installation,
-    },
-  ])
-  .select("*").single();
-
-  console.log({lead, error})
-
-
-
-     let encodedAddress;
-
-     if(typeof address !== "string") {
-       return
-    } else {
-      encodedAddress = encodeURIComponent(address)
-     }
-
-    const gecodingUrl = new URL(
-      `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}&key=${process.env.NEXT_PUBLIC_MAPS_API_KEY}`
-    );
-
-     const response = await fetch(gecodingUrl)
-       .then((res) => res.json())
-       .then((data) => data)
-       .catch((err) => console.error(err));
-
-      if (!response || response.status === "ZERO_RESULTS") {
-        redirect(`/error`);
-        // throw new Error("Could not find location for the specified address.");
-      }
-
-      const data: GeocodingResponse = await response;
-
-      const {data:addressResults, error:addressError} = await supabase.from("geocoding").insert([{
-       results: data.results,
-       status: data.status,
-       lead_id: lead?.id
-      }]).select("*").single()
-
-
-
-      if(addressError) {
-        // throw new Error("Could not save address")
-        redirect(`/error`)
-
-      }
-
-      if(!addressResults) {
-        // throw new Error("Could not save address")
-        redirect(`/error`)
-      }
-
-      redirect(`/calculate/${addressResults.id}`)
-
-      // redirect(`/calculate?lat=${data.results[0].geometry.location.lat}&lng=${data.results[0].geometry.location.lng}&address=${data.results[0].formatted_address}`)
-
+    terms: {
+      offset: number
+      value: string
+    }[]
+    types: string[]
   }
+}
+
+const AddressForm =  () => {
+
+    const [value, setValue] = useState<AutoCompleteType | null>(null);
+
+    console.log(value);
+
 
   return (
     <div className="grid grid-cols-1 gap-6 py-10 mx-auto lg:grid-cols-2">
@@ -142,26 +90,26 @@ const { data: lead, error } = await supabase
               required
               type="tel"
             /> */}
-                {/* <Autocomplete /> */}
-          </div>
-          <div className="space-y-2">
-            <Label htmlFor="address">Address</Label>
-            <Input
-              id="address"
-              name="address"
-              placeholder="Enter your address"
-              required
+            <Label htmlFor="address">Enter Street Address</Label>
+            <GooglePlacesAutocomplete
+              apiKey={process.env.NEXT_PUBLIC_MAPS_API_KEY!}
+              selectProps={{
+                value,
+                onChange: setValue,
+              }}
             />
+            <Input type="hidden" name="address" value={value?.label} />
           </div>
+
           <div className="space-y-2">
-            <Label htmlFor="electricity-bill">Monthly Electricity Bill</Label>
+            {/* <Label htmlFor="electricity-bill">Monthly Electricity Bill</Label>
             <Input
               id="electricity-bill"
               name="electricity-bill"
               // placeholder="Enter your address"
               required
               type="number"
-            />
+            /> */}
           </div>
           <div className="space-y-2">
             <Label>
