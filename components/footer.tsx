061fe69a729c1b8ce26d732be0f402668387e246
Philani Ncube <ncbphi001@gmail.com>
2023-12-12 22:51:29 +0200

    Update footer link styling

--- components/footer.tsx
+++ components/footer.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function Footer() {
   return (
     <footer className="w-full py-6 bg-gray-800 text-center text-white flex items-center justify-center">
-      <Link href="mailto:ncbphi001@gmail.com" className="text-sm"><Mail /> Philani Ncube</Link>
+      <Link href="mailto:ncbphi001@gmail.com" className="text-sm flex items-center justify-center space-x-3"><Mail /> Philani Ncube</Link>
     </footer>
   );
 }
