0f81e0e73886e7994ec88d180fa4dac547cd8369
Philani Ncube <ncbphi001@gmail.com>
2023-12-12 22:46:12 +0200

    Update metadata in layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,22 @@
 import Navbar from '@/components/navigation/navbar';
 import './globals.css'
 import Footer from '@/components/footer';
+import { Metadata } from 'next';
 
 
-export const metadata = {
+export const metadata:Metadata = {
   title: 'Radiant Potential',
   description: "Get some information about your home's solar potential",
+  keywords: "Solar Installations, Solar, Solar Power",
+  robots: "index, follow",
+  openGraph: {
+    images: "images/home.png",
+    type: 'website',
+    locale: 'en_ZA',
+    title: 'Radiant Potential',
+    description: "Get some information about your home's solar potential",
+  },
+
 }
 
 export default function RootLayout({
