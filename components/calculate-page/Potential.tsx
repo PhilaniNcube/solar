64eb8f489b29a41e91f3adf74ba0058f4b545fb5
Philani Ncube <ncbphi001@gmail.com>
2024-02-18 18:39:38 +0200

    Refactor GoogleMapsEmbed in Potential component

--- components/calculate-page/Potential.tsx
+++ components/calculate-page/Potential.tsx
@@ -23,23 +23,23 @@ export default function Potential({
 
 
   return (
-    <section className="w-full grid grid-cols-1 lg:grid-cols-2 gap-8">
+    <section className="grid w-full grid-cols-1 gap-8 lg:grid-cols-2">
       <GoogleMapsEmbed
-        className="w-full aspect-video"
+        // className="w-full aspect-video"
         style="width:100%;height:100%; aspect-ratio: 16/12;"
         allowfullscreen
         loading="lazy"
-        zoom={20}
-        apiKey={process.env.NEXT_PUBLIC_MAPS_API_KEY!}
+        zoom="20"
+        apiKey={process.env.NEXT_PUBLIC_MAPS_API_KEY || ""}
         center={`${coords.lat},${coords.lng}`}
         // height={550}
         // width={700}
         mode="place"
-        mapType="satellite"
+        maptype="satellite"
         q={`${address}`}
       />
       <div className="w-full">
-        <h1 className="text-4xl font-bold mb-4">Address Details</h1>
+        <h1 className="mb-4 text-4xl font-bold">Address Details</h1>
       </div>
     </section>
   );
